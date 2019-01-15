211f2091d52fbf95df5e1efe688135820b81d87d
nlh774 <nlh774>
2019-01-15 15:12:59 +0800

    1.升级DotNettyRPC nuget组件 到1.5.4，解决bug“decmail请求参数报错——可能是json。”，方法签名：
    public interface IOrderService
        {
            decimal CalculateFinalOrderSum(long userId, decimal originalSum);
        }
    
    我和原作者反馈、他解决的https://github.com/Coldairarrow/DotNettyRPC

--- ConAppDotNettyRPCTest.Client/Program.cs
+++ ConAppDotNettyRPCTest.Client/Program.cs
@@ -7,20 +7,20 @@ namespace ConAppDotNettyRPCTest.Client
     {
         static void Main(string[] args)
         {
-            //IHello client = RPCClientFactory.GetClient<IHello>("127.0.0.1", 9999);
-            //var msg1 = client.SayHello("Hello1");
-            //Console.WriteLine(msg1);
+            IHello client = RPCClientFactory.GetClient<IHello>("127.0.0.1", 9999);
+            var msg1 = client.SayHello("Hello1");
+            Console.WriteLine(msg1);
 
-            //var msg2 = client.SayHello("Hello2");
-            //Console.WriteLine(msg2);
+            var msg2 = client.SayHello("Hello2");
+            Console.WriteLine(msg2);
 
             decimal prize = 100.1M;
             IOrderService orderService = RPCClientFactory.GetClient<IOrderService>("127.0.0.1", 9999);
             var sum1 = orderService.CalculateFinalOrderSum(500, prize);
             Console.WriteLine(sum1);
 
-            //var sum2 = orderService.CalculateFinalOrderSum(11000, 1000);
-            //Console.WriteLine(sum2);
+            var sum2 = orderService.CalculateFinalOrderSum(11000, 1000);
+            Console.WriteLine(sum2);
 
             Console.ReadLine();
         }
